a3bc55c3b93ceb52f78ec28a5d65ba407c791808
王嘉宁 <evilrabbit520@gmail.com>
2018-01-12 13:08:10 +0800

    基本成形

--- Google_guide/Controllers/HomeController.cs
+++ Google_guide/Controllers/HomeController.cs
@@ -17,7 +17,11 @@ namespace Google_guide.Controllers
         {
             return View();
         }
-        public ActionResult Article()     //文章内容页面
+        public ActionResult Article_Google_1()     //文章内容页面
+        {
+            return View();
+        }
+        public ActionResult Article_Google_2()     //未使用
         {
             return View();
         }
